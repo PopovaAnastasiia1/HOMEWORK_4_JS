1eb7cc2cb2d0154aaf5600cabc75d6ed68d6afec
Anastasiia Popova <popovaanastasiia123@gmail.com>
2023-01-24 20:42:18 +0200

    corrected work

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
-let firstNum = +prompt("Введіть перше число");
-while (firstNum === "" || Number(isNaN(firstNum))) {
+function doMathOperation(firstNum, secondNum, operant) {
+  firstNum = +prompt("Введіть перше число");
+  while (firstNum === "" || Number(isNaN(firstNum))) {
     firstNum = +prompt("Введіть перше число ще раз:");
-}
+  }
 
-let secondNum = +prompt("Введіть друге число");
-while (secondNum === "" || Number(isNaN(secondNum))) {
+  secondNum = +prompt("Введіть друге число");
+  while (secondNum === "" || Number(isNaN(secondNum))) {
     secondNum = +prompt("Введіть друге число ще раз:");
-}
+  }
 
-let operant = prompt(
-  "Введіть математичну операцію, яку потрібно виконати. Сюди може бути введено +, -, *, /."
-);
-while (!/^[\-+*(/)]/.test(operant)) {
-    operant = prompt("Введіть математичну операцію, яку потрібно виконати ще раз. Сюди може бути введено +, -, *, /.");
-}
+  operant = prompt(
+    "Введіть математичну операцію, яку потрібно виконати. Сюди може бути введено +, -, *, /."
+  );
+  while (!/^[\-+*(/)]/.test(operant)) {
+    operant = prompt(
+      "Введіть математичну операцію, яку потрібно виконати ще раз. Сюди може бути введено +, -, *, /."
+    );
+  }
 
-
-function doMathOperation() {
-  if (operant === "+") {
-    console.log(`${firstNum} + ${secondNum} = ${firstNum + secondNum}`);
-  } else  if (operant === "-"){
-    console.log(`${firstNum} - ${secondNum} = ${firstNum - secondNum}`);
-  }else  if (operant === "*"){
-    console.log(`${firstNum} * ${secondNum} = ${firstNum * secondNum}`);
-  }else  if (operant === "/"){
-    console.log(`${firstNum} / ${secondNum} = ${firstNum / secondNum}`);
+  switch (operant) {
+    case "+": {
+      return firstNum + secondNum;
+    }
+    case "-": {
+      return firstNum - secondNum;
+    }
+    case "*": {
+      return firstNum * secondNum;
+    }
+    case "/": {
+      return firstNum / secondNum;
+    }
   }
 }
-
-doMathOperation();
-
+console.log(doMathOperation('firstNum', 'secondNum', 'operant'));
